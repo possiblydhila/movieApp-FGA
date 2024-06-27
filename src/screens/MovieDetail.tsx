d99e2ffa157b6f6a08a9837a2bde614dcf1123f3
possiblydhila <fadhilapurboningrum@gmail.com>
2024-06-27 15:02:16 +0700

    set up env & dev movie detail

--- src/screens/MovieDetail.tsx
+++ src/screens/MovieDetail.tsx
@@ -1,36 +1,30 @@
-import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native'
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import { NavigationProp } from '@react-navigation/native';
+import React from 'react'
+import { View, Text, Button } from 'react-native'
+import { API_URL, API_ACCESS_TOKEN } from '@env' // Ditambahkan
 
 const MovieDetail = ({ navigation }: any): any => {
-    const fetchData = (): void => {
-        const ACCESS_TOKEN = process.env.EXPO_PUBLIC_API_ACCESS_TOKEN
-        const URL = process.env.EXPO_PUBLIC_API_URL
-    
-        if (ACCESS_TOKEN == null || URL == null) {
-          throw new Error('ENV not found')
-        }
-    
-        const options = {
-          method: 'GET',
-          headers: {
-            accept: 'application/json',
-            Authorization: `Bearer ${ACCESS_TOKEN}`,
-          },
-        }
-    
-        fetch(URL, options)
-          .then(async (response) => await response.json())
-          .then((response) => {
-            console.log(response)
-          })
-          .catch((err) => {
-            console.error(err)
-          })
-      }
-  
+  const fetchData = (): void => {
+    if (API_URL == null || API_ACCESS_TOKEN.length == null) {
+      throw new Error('ENV not found')
+    }
+
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${API_ACCESS_TOKEN}`,
+      },
+    }
+
+    fetch(API_URL, options)
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response)
+      })
+      .catch((err) => {
+        console.error(err)
+      })
+  }
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <Text>Movie Detail Page</Text>
