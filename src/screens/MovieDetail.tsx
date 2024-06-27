9256ff560be3bb898b1f3ed6798e70bb72b0ef39
possiblydhila <fadhilapurboningrum@gmail.com>
2024-06-27 14:56:30 +0700

    set up env

--- src/screens/MovieDetail.tsx
+++ src/screens/MovieDetail.tsx
@@ -4,26 +4,44 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { NavigationProp } from '@react-navigation/native';
 
-interface Props {
-  navigation: NavigationProp<any, any>;
-}
-
-export default function MovieDetail({ navigation }: Props): JSX.Element {
-  return (
-    <View style={styles.container}>
-        <Text>MOVIE DETAIL</Text>
-      <Button title="Go Back" onPress={() => navigation.navigate('Home')} />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    text: {
-      marginBottom: 20, // Add some space between the text and button
-    },
-  });
+const MovieDetail = ({ navigation }: any): any => {
+    const fetchData = (): void => {
+        const ACCESS_TOKEN = process.env.EXPO_PUBLIC_API_ACCESS_TOKEN
+        const URL = process.env.EXPO_PUBLIC_API_URL
+    
+        if (ACCESS_TOKEN == null || URL == null) {
+          throw new Error('ENV not found')
+        }
+    
+        const options = {
+          method: 'GET',
+          headers: {
+            accept: 'application/json',
+            Authorization: `Bearer ${ACCESS_TOKEN}`,
+          },
+        }
+    
+        fetch(URL, options)
+          .then(async (response) => await response.json())
+          .then((response) => {
+            console.log(response)
+          })
+          .catch((err) => {
+            console.error(err)
+          })
+      }
+  
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Movie Detail Page</Text>
+        <Button
+          title="Fetch Data"
+          onPress={() => {
+            fetchData()
+          }}
+        />
+      </View>
+    )
+  }
+  
+  export default MovieDetail
